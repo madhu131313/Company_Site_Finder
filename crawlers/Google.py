df49999093d61733e8ccc8fc556f73d49fd461ed
Madhu Sudhan <madhu131313@gmail.com>
2017-08-20 01:21:54 +0530

    modularized code

---
+++ crawlers/Google.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import json
+
+with open('config.json') as json_data_file:
+    data = json.load(json_data_file)
+print(data)
+
+def Crawler(coName):
+	driver = webdriver.PhantomJS(data['PHANTOMJS_PATH'])
+	url = "https://www.google.co.in/search?q="+ coName 
+	driver.get(url)
+	driver.save_screenshot('link.png')
+	soup = BeautifulSoup(driver.page_source,"html.parser")
+	results = soup.find_all('div',{'class':'g'}, limit=3)
+	for result in results:
+                #print result
+		title =  result.find('h3', {'class':'r'}).find('a').text
+		print title
+		domain = result.find('cite').text.split("//")[-1].split("/")[0]
+		print domain
